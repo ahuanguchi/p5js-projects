53db752f8e1bf19faa53282d255516ede551c30f
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-06-01 23:15:17 -0500

    Added waveform visualization

--- keyboardsynth/keyboardsynth.js
+++ keyboardsynth/keyboardsynth.js
@@ -1,4 +1,5 @@
 var wave;
+var fft;
 var currentFreq;
 var currentKey;
 
@@ -51,40 +52,46 @@ var bgColor;
 function setup() {
   createCanvas(512, 200);
   cursor(CROSS);
-  bgColor = Math.floor(map(currentAmp, 0, 1, 0, 64));
+  strokeWeight(1);
+  noFill();
+  bgColor = floor(map(currentAmp, 0, 1, 0, 64));
   wave = new p5.TriOsc();
   wave.amp(0);
   wave.start();
+  fft = new p5.FFT();
   
   waveMap[LEFT_ARROW] = p5.SinOsc;
   waveMap[DOWN_ARROW] = p5.TriOsc;
   waveMap[RIGHT_ARROW] = p5.SawOsc;
   waveMap[UP_ARROW] = p5.SqrOsc;
   waveMap[SHIFT] = p5.Pulse;
-  
-  stroke(255);
 }
 
 function draw() {
+  var i;
+  var waveform = fft.waveform();
+  var len = waveform.length;
   background(bgColor);
-  // stroke(127);
-  // for (int i = 0; i < bufferSize - 1; i++) {
-    // float x1 = map(i, 0, bufferSize, 0, width);
-    // float x2 = map(i + 1, 0, bufferSize, 0, width);
-    // line(x1, 50 + out.left.get(i) * 50, x2, 50 + out.left.get(i + 1) * 50);
-    // line(x1, 150 + out.right.get(i) * 50, x2, 150 + out.right.get(i + 1) * 50);
-  // }
+  beginShape();
+  stroke(127);
+  for (i = 0; i < len; i += 1) {
+    var x = map(i, 0, len, 0, width);
+    var y = map(waveform[i], -1, 1, 0, height);
+    vertex(x, y);
+  }
+  endShape();
   
+  stroke(255);
   text("Hz: " + currentFreq, 5, 15);
   text("Octave: " + octave, 5, 30);
   text("Press z-/ and q-] to play notes (z and q are C). Accidentals are on the row above.", 5, height - 55);
   text("Press + and - to change octaves. Hold \\ to toggle octaves mid-note.", 5, height - 40);
   text("Move your mouse to control the volume (right is louder).", 5, height - 25);
-  text("Use the arrow keys, and shift to pick a waveform.", 5, height - 10);
+  text("Use the arrow keyse and shift to pick a waveform.", 5, height - 10);
 }
 
 function keyPressed() {
-  var noteNum = noteMap[keyCode >= 0x30 && keyCode <= 0x5A ? key : keyCode];
+  var noteNum = noteMap[keyCode <= 0x5A ? key : keyCode];
   if (key !== currentKey && noteNum !== undefined) {
     currentFreq = toHz(noteNum);
     wave.freq(currentFreq);
@@ -139,7 +146,7 @@ function keyReleased() {
 
 function mouseMoved() {
   currentAmp = constrain(map(mouseX, 0.0, width, 0.0, 1.0), 0, 1);
-  bgColor = Math.floor(map(currentAmp, 0, 1, 0, 64));
+  bgColor = floor(map(currentAmp, 0, 1, 0, 64));
   if (active) {
     wave.amp(currentAmp, 0.01);
   }
